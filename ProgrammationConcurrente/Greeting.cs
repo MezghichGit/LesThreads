35c2515f5297af8ecbbadc18d78f10a6f5ab25b7
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-01-05 11:41:40 +0100

    Les Thread en C#

---
+++ ProgrammationConcurrente/Greeting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+namespace ProgrammationConcurrente
+{
+    class Greeting
+    {
+        // static method one
+        public static void method1()
+        {
+       
+            for (int I = 0; I <= 10; I++)
+            {
+                Console.WriteLine("Method1 is : {0}", I);
+                // When the value of I is equal to
+                if (I == 5)
+                {
+                    Thread.Sleep(6000); // un temps d'attente(Method1 est bloquée)"
+                }
+            }
+        }
+
+        // static method two
+        public static void method2()
+        {
+            // It prints numbers from 0 to 10
+            for (int J = 0; J <= 10; J++)
+            {
+                Console.WriteLine("Method2 is : {0}", J);
+            }
+        }
+    }
+}
---
+++ ProgrammationConcurrente/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProgrammationConcurrente
+{
+    class Program
+    {
+       
+        static string mot = "Lorem";
+        static string textFile = @"C:\Users\amine\Desktop\Lab\monText.txt";
+        static string texte = read_text();
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("main");
+
+            Thread thr1 = new Thread(nbreOccurenceWord);
+            Thread thr2 = new Thread(replaceSpaceByAsterisk);
+            thr1.Start();
+            thr2.Start();
+
+
+
+        }
+
+
+        static void nbreOccurenceWord()
+        {
+            //Thread.Sleep(6000);// Ficher top voluminueux
+            Console.WriteLine("calcul nombre occurences");
+            
+            int k = 0;
+            //var splitted_text = texte.Split("*");
+            var splitted_text = texte.Split(" ");
+            foreach (var word in splitted_text)
+            {
+                if (word == mot)
+                {
+                    k++;
+
+                }
+            }
+            Console.WriteLine("Le nombre d'occurrences est:" + k);
+        }
+        static void replaceSpaceByAsterisk()
+        {
+            Console.WriteLine("remplacement...");
+            string new_text = read_text();
+            new_text = texte.Replace(" ", "*");
+            File.WriteAllText(textFile, new_text);
+
+        }
+
+
+        static string read_text()
+        {
+
+
+            // Read entire text file content in one string    
+            string text = File.ReadAllText(textFile);
+
+
+
+            return text;
+
+
+        }
+
+        
+
+
+        
+
+    }
+
+    
+}
